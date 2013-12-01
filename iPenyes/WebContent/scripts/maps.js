5df4e4447036be7e829f0f2b318f69fdd34265f9
osoler <oscarsolermora@gmail.com>
2013-12-01 20:00:29 +0100

    random map

--- iPenyes/WebContent/scripts/maps.js
+++ iPenyes/WebContent/scripts/maps.js
@@ -15,7 +15,10 @@ $(document).on('pageshow', '#mapPenyes2',function(e,data){
 	createMap();
 });
 
-
+function addMarker(map, title, x, y){
+	 var myLatlng = new google.maps.LatLng(x,y);
+	 new google.maps.Marker({position: myLatlng,map: map,title:title,animation: google.maps.Animation.DROP});
+}
 function createMap(){	
 	$('#map_canvas').empty();
 	
@@ -25,24 +28,28 @@ function createMap(){
 	
 	
     var myLatlng = new google.maps.LatLng(60,17);
-    var myLatlng2 = new google.maps.LatLng(60,18);
-    var myLatlng3 = new google.maps.LatLng(61,17);
-    var myLatlng4 = new google.maps.LatLng(60,17.1);
     
     var mapOptions = {
-    	    zoom: 6,
+    	    zoom: 9,
     	    center: myLatlng,
     	    mapTypeId: google.maps.MapTypeId.ROADMAP,
     	    disableDefaultUI: true};
     
     var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);    
     
-    new google.maps.Marker({position: myLatlng,map: map,title:"Hello Penyes1!",animation: google.maps.Animation.DROP});
-    new google.maps.Marker({position: myLatlng2,map: map,title:"Hello Penyes 2!",animation: google.maps.Animation.DROP});
-    new google.maps.Marker({position: myLatlng3,map: map,title:"Hello Penyes 3!",animation: google.maps.Animation.DROP});
-    new google.maps.Marker({position: myLatlng4,map: map,title:"Hello Penyes 3!",animation: google.maps.Animation.DROP});
-
+    addMarker(map, title,60,17);
+    for (var n = 1; n <= 30; n++) {
+    	var title = "Hello Penyes " + n ;
+        var randX = Math.random();
+        var randY = Math.random();
+        randX *= (randX * 1000000) % 2 == 0 ? 1 : -1;
+        randY *= (randY * 1000000) % 2 == 0 ? 1 : -1;	
+    	addMarker(map, title,60 + (randX * 0.5),17 + (randY * 0.5));
+    }
+    
 }
+
+
 
 function getRealContentHeight() {
 	var header = $.mobile.activePage.find("div[data-role='header']:visible");

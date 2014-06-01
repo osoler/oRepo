364ea24203b2f86d3de1abfbe9b3fa9fd7f22cf2
osoler <oscarsolermora@gmail.com>
2014-06-01 15:18:31 +0200

    fixed photo problems 2

--- iPenyes/WebContent/scripts/photo.js
+++ iPenyes/WebContent/scripts/photo.js
@@ -3,6 +3,13 @@ var photoPenyes = function () {
 	
 	function showHideCross() {
 		  var currentScale = window.outerWidth;
+
+		  if (this.orientation === 0){
+			  currentScale = window.outerWidth;
+		  }else{
+			  currentScale = window.outerHeight;
+		  }
+
 		  if (currentScale === 320)
 		  {
 		  	$("#photoCloser").show();
